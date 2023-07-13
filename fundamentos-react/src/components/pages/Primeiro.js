9123ddff90ce5f989efd60094e500fdd917ffe98
ViniciusfAraujo <128641765+ViniciusfAraujo@users.noreply.github.com>
2023-07-13 09:33:53 -0300

    Aula Inicializada

---
+++ fundamentos-react/src/components/pages/Primeiro.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Primeiro(){
+    const msg = 'Hello World'
+    return (
+        <div>
+            <h1>Primeiro componente</h1>
+            <p>{msg}</p>
+        </div>
+    )
+}
+export default Primeiro
---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react' //JSX
+
+import Primeiro from './components/pages/Primeiro'
+
+
+ReactDOM.render(
+    <div>
+        <Primeiro/>
+    </div>,
+    document.getElementById('root')
+)
